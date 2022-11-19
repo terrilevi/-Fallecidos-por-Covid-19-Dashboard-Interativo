a03b9953306d587a1b46eef70f8fe6f9328d49fd
unknown <anderson.rojas1805@gmail.com>
2022-11-19 18:39:25 -0500

    Primer commit de streamlit

--- codigo.py
+++ codigo.py
@@ -1,6 +1,14 @@
-import streamlit as st
+import streamlit as st 
+import pandas as pd
+import numpy as np
 
-st.tittle("Streamlit Sliders")
-st.subheader("Slider 1:")
-x = st.slider('A number between 0-100')
-st.write("Slider number:", x)
+st.title('Titulo del Proyecto')
+
+color = st.select_slider(
+    'Select a color of the rainbow',
+    options=['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+st.write('My favorite color is', color)
+
+color = st.color_picker('Pick A Color', '#00f900')
+st.write('The current color is', color)
+
